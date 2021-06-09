fb3add1f116bf95e20078801538e5e4de03b5c3a
Piotr Hendzel <pihe@jcommerce.pl>
2021-06-09 22:35:51 +0200

    Add JMH benchmark

---
+++ src/test/java/pl/hendzel/searchappjdk8/SearchAppJdk8ApplicationTests.java
@@ -0,0 +1,28 @@
+package pl.hendzel.searchappjdk8;
+
+import org.junit.jupiter.api.Test;
+import org.openjdk.jmh.results.format.ResultFormatType;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class SearchAppJdk8ApplicationTests {
+
+	@Test
+	void contextLoads() throws RunnerException {
+		Options options = new OptionsBuilder()
+				.include(".*StringsBenchmark")
+				.addProfiler(GcProfiler.class)
+				.addProfiler(ForcedGcMemoryProfiler.class)
+				.result("result.csv")
+				.resultFormat(ResultFormatType.CSV)
+				.warmupIterations(3)
+				.measurementIterations(5)
+				.forks(1)
+				.build();
+		new Runner(options).run();
+	}
+
+}
