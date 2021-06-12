4830549f21c380f8854e29fc078db00fac84cb86
Piotr Hendzel <pihe@jcommerce.pl>
2021-06-12 10:26:23 +0200

    Clean code

--- src/test/java/pl/hendzel/searchappjdk8/SearchAppJdk8ApplicationTests.java
+++ src/test/java/pl/hendzel/searchappjdk8/SearchAppJdk8ApplicationTests.java
@@ -6,7 +6,6 @@ import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
-import org.springframework.boot.test.context.SpringBootTest;
 
 class SearchAppJdk8ApplicationTests {
 
